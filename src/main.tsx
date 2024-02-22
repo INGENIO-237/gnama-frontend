3dcdff83b4647fffa03ccc66821ddb319d05a850
INGENIO <ingeniokhalif@gmail.com>
2024-02-22 12:19:03 +0100

    set up aut0 authentication system

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
+import CustomAuth0Provider from "./auth/CustomAuth0Provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <CustomAuth0Provider>
+        <AppRoutes />
+      </CustomAuth0Provider>
     </Router>
   </React.StrictMode>
 );
