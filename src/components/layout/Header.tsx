85028cb75f5bac51bbebc946779b345336078522
INGENIO <ingeniokhalif@gmail.com>
2024-02-22 09:01:16 +0100

    added main nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 export default function Header() {
   return (
     <div className="border-b-2 border-b-orange-500 py-5">
       <div className="container flex mx-auto justify-between item-center">
-        <Link className="text-3xl font-bold tracking-tight text-orange-500" to="/">
+        <Link
+          className="text-3xl font-bold tracking-tight text-orange-500"
+          to="/"
+        >
           Gnama
         </Link>
 
         <div className="md:hidden">
-            <MobileNav />
+          <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
