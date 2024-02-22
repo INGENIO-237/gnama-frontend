05bfa985889140ca4c4117b9bfbc843d84e8d961
INGENIO <ingeniokhalif@gmail.com>
2024-02-22 08:54:55 +0100

    added mobile nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 export default function Header() {
   return (
@@ -7,6 +8,10 @@ export default function Header() {
         <Link className="text-3xl font-bold tracking-tight text-orange-500" to="/">
           Gnama
         </Link>
+
+        <div className="md:hidden">
+            <MobileNav />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/layout/MobileNav.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-6">
+        <SheetTitle>
+          <span>Welcome on Gnama</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+}
